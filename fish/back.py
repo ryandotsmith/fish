eb52f1d613f465a04f17d52b52113e71267fb35c
♠ ace hacker <ryan@heroku.com>
2012-11-15 00:08:37 -0800

    add new files

---
+++ fish/back.py
@@ -0,0 +1,33 @@
+import argparse, os
+from db import DB
+from flask import Flask, redirect, request
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--address')
+parser.add_argument('--buddy', default=None)
+parser.add_argument('--dir')
+parser.add_argument('--priority', type=int, default=0)
+parser.add_argument('--want-master', action="store_true", default=False)
+a = parser.parse_args()
+
+db = DB(a.dir, a.address, a.priority, a.buddy, a.want_master, False)
+app = Flask(__name__)
+
+@app.route('/is-master')
+def is_master():
+  if db.is_master():
+    return('yes')
+  else:
+    return('no')
+
+@app.route('/receive', methods=['PUT'])
+def receive():
+  if db.is_master():
+    return('at=accept body=%s' % request.form['data'])
+  else:
+    addr = db.master_address()
+    return('at=deny master=%s' % addr)
+
+if __name__ == '__main__':
+  port = int(os.environ.get('PORT', 5000))
+  app.run(host='0.0.0.0', port=port)
